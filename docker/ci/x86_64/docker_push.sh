0ad1e16adbf11f8d8d2a7b18cd5de5634fe8d654
mmgmx <meissatan@gmail.com>
2022-03-21 19:21:49 +0100

    Enable docker push

--- docker/ci/x86_64/docker_push.sh
+++ docker/ci/x86_64/docker_push.sh
@@ -4,11 +4,11 @@ DOCKER_TAGS=""
 
 for TAG in "$@"
 do
-	DOCKER_TAGS="$DOCKER_TAGS -t stashapp/stash:$TAG"
+	DOCKER_TAGS="$DOCKER_TAGS -t avx1024/stash:$TAG"
 done
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 # must build the image from dist directory
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6 --push $DOCKER_TAGS -f docker/ci/x86_64/Dockerfile dist/
+docker buildx build --platform linux/amd64,linux/arm64 --push $DOCKER_TAGS -f docker/ci/x86_64/Dockerfile dist/
 
